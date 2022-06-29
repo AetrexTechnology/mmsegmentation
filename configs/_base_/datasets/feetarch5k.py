27a9c53f8f9c93604a4958277fe2549b1ddec24f
ZhimingNJ <107000182+ZhimingNJ@users.noreply.github.com>
2022-06-29 10:44:03 -0400

    Update feetarch5k.py

--- configs/_base_/datasets/feetarch5k.py
+++ configs/_base_/datasets/feetarch5k.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'MyFeetArchDataset'
-data_root = 'data/feet_scans'
+data_root = 'data/arch_scans_324'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 512)
