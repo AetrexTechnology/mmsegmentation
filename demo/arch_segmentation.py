b9ff3d5d98362ab716b5b2f53a93b97057c996a2
Zhiming Liu <zliu@aetrex.com>
2022-06-28 22:25:02 -0400

    python script for arch boundary segmentation

--- demo/arch_segmentation.py
+++ demo/arch_segmentation.py
@@ -6,6 +6,11 @@ import mmcv
 import cv2
 import numpy as np
 
+'''
+This python script run in Windows system.
+It may need a few modification for running in Linux system
+'''
+
 def extract_top_boundary(seg_img):
     seg_img = seg_img.astype(np.uint8)
 
@@ -26,7 +31,7 @@ def segmentor(img_name, model=None, args=None):
     # test a single image
     result = inference_segmentor(model, img_name)
     result,boundary = extract_top_boundary(result[0])
-    out_img_name = os.path.join(args.show_dir, img_name.split('\\')[-1])
+    out_img_name = os.path.join(args.show_dir, img_name.split('\\')[-1])  # path seperator for Windows
     model.show_result(
         img_name, [result], palette=get_palette(args.palette), show=False, opacity=args.opacity, out_file=out_img_name)
 
@@ -36,8 +41,8 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('config', help='Config file')
     parser.add_argument('checkpoint', help='Checkpoint file')
-    parser.add_argument('--img-path', help='the path for images to be segmented')
-    parser.add_argument('--show-dir', help='the path for result images')
+    parser.add_argument('--img-path', help='The path for images to be segmented')
+    parser.add_argument('--show-dir', help='The path for output images')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
@@ -69,6 +74,7 @@ def main():
     kwargs = {'model': model, 'args': args}
     results = mmcv.track_progress(segmentor, imgs_name_list, **kwargs)
 
+    # all top boundary of arch are contained in this pkl file
     output_file = os.path.join(args.show_dir, 'output.pkl')
     mmcv.dump(results,output_file)
 
